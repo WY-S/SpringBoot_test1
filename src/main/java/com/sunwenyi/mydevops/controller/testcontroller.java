1525f7052143d74d4d273ba9cfb9d49c3798eea9
WenyiSun <646441755@qq.com>
2022-08-09 23:06:39 +0800

    First Commit 2022/8/9 23:05pm

---
+++ src/main/java/com/sunwenyi/mydevops/controller/testcontroller.java
@@ -0,0 +1,15 @@
+package com.sunwenyi.mydevops.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testcontroller {
+
+    @RequestMapping("/hello")
+    public String test(){
+        return "Hello Jenkins!!";
+    }
+
+}
